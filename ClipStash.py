9fcbc0a92e1e48b345addb758ae9e183a96e4611
Sparkmasterflex <raymondke99@gmail.com>
2015-07-26 12:10:29 -1000

    Formatting and single line
    used timestamp and file name/location to wrap around copied selection
    
    added code to recognize no selection and copy/cuts current line

--- ClipStash.py
+++ ClipStash.py
@@ -1,4 +1,9 @@
 import sublime, sublime_plugin
+import time
+import os
+from os.path import dirname, realpath
+
+CLIPBOARD = os.path.join(sublime.packages_path(), os.path.dirname(os.path.realpath(__file__)), 'clipstash-clipboard.txt')
 
 class ClipStash(sublime_plugin.TextCommand):
   def window(self):
@@ -7,47 +12,56 @@ class ClipStash(sublime_plugin.TextCommand):
   def get_selection(self):
     lines = []
     self.region_set = self.view.sel()
-    for region in self.region_set:
-      lines.append(self.view.substr(region))
+    if self.region_set[0].empty():
+      line = self.view.line(self.region_set[0])
+      lineContents = self.view.substr(line) + '\n'
+      lines.append(lineContents)
+    else:
+      print "not at all"
+      for region in self.region_set:
+        lines.append(self.view.substr(region))
     return lines
 
-  def open_new_file(self):
-    return self.window().new_file()
-
-  def allow_file_edit(self):
-    self.clip_board.set_read_only(False)
-    return self.clip_board.begin_edit()
-
-  def close_edit(self, edit):
-    self.clip_board.end_edit(edit)
-
-  def paste_in(self):
-    edit = self.allow_file_edit()
-    self.clip_board.insert(edit, 0, "".join(self.lines))
-    self.close_edit(edit)
+  def open_paste_buffer(self):
+    self.clip_board = self.window().open_file(CLIPBOARD)
+    self.edit = self.clip_board.begin_edit()
 
 
+  def close_edit(self):
+    self.clip_board.end_edit(self.edit)
 
+  def paste_in(self, cut=False):
+    timestamp = time.strftime("%m-%d-%Y %I:%M:%S")
+    name = "Unsaved File" if self.view.name() == "" else self.view.name()
+    label  = "Cut" if cut else "Copied"
+    label += " from " + name
+
+    if self.view.file_name() != None:
+      self.clip_board.insert(self.edit, 0, "\n\n=========="+ self.view.file_name() +"\n\n\n")
+    else:
+      self.clip_board.insert(self.edit, 0, "\n\n\n")
+    self.clip_board.insert(self.edit, 0, "\n\n".join(self.lines))
+    self.clip_board.insert(self.edit, 0, "============  "+ label +": "+ timestamp +"  ============\n\n")
+    self.close_edit()
+
+
+# ClipStash Copy Command
 class ClipStashCopyCommand(ClipStash):
   def run(self, edit):
     self.lines = self.get_selection()
-    self.clip_board = self.open_new_file()
-    edit = self.allow_file_edit()
+    self.open_paste_buffer()
     self.paste_in()
-    self.close_edit(edit)
 
 
-
+# ClipStash Cut Command
 class ClipStashCutCommand(ClipStash):
   def run(self, edit):
     self.lines = self.get_selection()
-    self.clip_board = self.open_new_file()
+    self.open_paste_buffer()
     self.delete_selected()
-    self.paste_in()
-    self.close_edit(edit)
+    self.paste_in(True)
 
   def delete_selected(self):
-    edit = self.allow_file_edit()
     for line in self.region_set:
-      self.view.erase(edit, line)
+      self.view.erase(self.edit, line)
 
